100b6983070d4e8e98751ce7d60b7c193b5ff4b7
daffaabp <daffa.budi2003@gmail.com>
2024-12-01 18:51:26 +0700

    testing public api

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,17 @@
-const HomePage = () => {
+import getBanners from "@/actions/get-banner";
+import Banner from "@/components/banner";
+import Container from "@/components/ui/container"
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const banner = await getBanners("4d434752-7aeb-4912-8990-8de61da31f17");
   return (
-    <div>
-      Home Page
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Banner data={banner} />
+      </div>
+    </Container>
   );
 }
 
